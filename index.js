638f395acdeb75091a72d0b5df1b643ee49cac7d
angel-torres <alas4224@gmail.com>
2019-06-06 23:38:54 -0500

    add read file functionality

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var _data = require('./lib/data');
 
 // TESTING
 // @TODO delete this
-_data.create('test', 'newFile', {'foo':'bar'}, function(err){
-    console.log('This was the error', err);
+_data.read('test', 'newFile', function(err, data){
+    console.log('This was the error', err, 'this is the data', data);
 });
 
 // Instantiating http server
